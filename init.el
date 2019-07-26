0abcaa1d5d597d4eddedf25e197a693dc017996b
girvain <gavinross88@hotmail.co.uk>
2019-07-26 23:33:02 +0100

    weird melpa issue fixed with init.el code snippet. Changed to lsp-mode

--- init.el
+++ init.el
@@ -2,11 +2,15 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-;; (add-to-list 'package-archives
+
+
+;;(add-to-list 'package-archives
 ;; 	     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
 ;; (add-to-list 'package-archives
 ;;	     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
 (package-initialize)
+
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
@@ -16,3 +20,20 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
+ '(package-selected-packages
+   (quote
+    (evil-escape evil-collection evil dashboard undo-tree neotree treemacs-magit treemacs-projectile treemacs-evil treemacs multi-term counsel-projectile projectile yasnippet-snippets yasnippet counsel ivy pug-mode tide rjsx-mode js2-mode prettier-js web-mode emmet-mode company flycheck-plantuml flycheck-pos-tip flycheck-color-mode-line flycheck cmake-mode highlight-indent-guides beacon org-bullets which-key try rainbow-delimiters doom-modeline all-the-icons doom-themes use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
