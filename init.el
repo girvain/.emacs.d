e95a9e1f3a8de1348efe75d3edb8f58a74d2c06e
gavin <gross@nn4m.com>
2025-04-17 07:58:01 +0100

    fzf notes function works now, thanks chatgpt

--- init.el
+++ init.el
@@ -649,30 +649,26 @@ There are two things you can do about this warning:
   (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
         fzf/executable "fzf"
         fzf/git-grep-args "-i --line-number %s"
-        ;; command used for `fzf-grep-*` functions
-        ;; example usage for ripgrep:
-        ;; fzf/grep-commanqd "rg --no-heading -nH"
         fzf/grep-command "grep -nrH"
-        ;; If nil, the fzf buffer will appear at the top of the window
         fzf/position-bottom t
         fzf/window-height 15)
 
-  (defun fzf-example ()
-    (fzf-with-entries
-     (list "a" "b" "c")
-     'print))
   (defun fzf-find-notes (&optional directory)
     (interactive)
-    (let ((d (fzf/resolve-directory directory)))
+    (let ((d (or directory default-directory)))
       (fzf
        (lambda (x)
          (let ((f (expand-file-name x d)))
            (when (file-exists-p f)
              (find-file f))))
        d)))
-  (defun my-notes ()
-    (interactive)
-    (fzf/resolve-directory "~/Dropbox")))
+
+(defun my-notes ()
+  (interactive)
+  (let ((default-directory (expand-file-name "~/Dropbox")))
+    (if (file-directory-p default-directory)
+        (fzf)
+      (message "Directory ~/Dropbox does not exist.")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Clojure
